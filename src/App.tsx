ef1b77b45c91fceab9cb67f07273e8e8bc69edbf
Rakib Hassan <mailrakibhassan@gmail.com>
2025-08-12 08:03:05 +0600

    inserting & reading tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
+import { supabase } from "./supabase-client";
 
 function App() {
   const [task, setTask] = useState({ title: "", description: "" });
+  const [tasks, setTasks] = useState([]);
 
-  const handleSubmit = (e) => {
+  const fetchTasks = async () => {
+    const { error, data } = await supabase
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+
     
-  }
+    if (error) {
+      console.log("Error adding task:", error);
+      return;
+    }
+    
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    // insert a new task to supabase
+    const { error } = await supabase.from("tasks").insert(task).single();
+
+    if (error) {
+      console.log("Error adding task:", error);
+      return;
+    }
+
+    // reset the form
+    setTask({ title: "", description: "" });
+  };
 
   return (
     <div>
       <h3>Task Manager CRUD</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           placeholder="Task Title"
           onChange={(e) => {
@@ -23,7 +50,7 @@ function App() {
             setTask((prev) => ({ ...prev, description: e.target.value }));
           }}
         />
-        <button>Add Task</button>
+        <button type="submit">Add Task</button>
       </form>
 
       <div>
--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabase = createClient(
+export const supabase = createClient(
     "https://czvpvlzdnozcjxjlryaz.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dnB2bHpkbm96Y2p4amxyeWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5MTcxMTgsImV4cCI6MjA3MDQ5MzExOH0.E6exTcZAs36Dy0JMekdvJfCxZiNmbto06uPmHhUAF-k"
 );
